07bd0f3e13ccb5690ccf35c5f6dc81a5a231c748
Nicolas Alpi <nicolas@cookieshq.co.uk>
2019-10-24 11:00:40 +0100

    Change Application Name for AppName

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,8 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module QuestionsWebApp
+#TODO change app_name
+module AppName
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
