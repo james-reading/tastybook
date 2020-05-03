9117c16531ca58b93e90ccf00d168b6af3ae5c94
James Reading <jamesreading473@gmail.com>
2020-05-03 11:36:38 +0100

    Add asset_host config for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,9 +27,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = ENV.fetch('ASSET_HOST', ENV.fetch('APPLICATION_HOST'))
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
