dc3f8da929b94f1359de3fd63bc873d1152b583b
James Reading <jamesreading473@gmail.com>
2020-09-13 08:52:11 +0100

    Refactor sortable steps to allow for sortable ingredients

--- app/webpack/javascript/modules/sortable.js
+++ app/webpack/javascript/modules/sortable.js
@@ -1,19 +1,18 @@
 import Sortable from 'sortablejs';
 
 document.addEventListener('turbolinks:load', () => {
-  const sortableList = document.getElementById('steps');
+  const sortableFields = document.querySelectorAll('.js-sortable-fields');
 
-  sortableList &&
-    new Sortable(sortableList, {
+  sortableFields.forEach(fields => {
+    new Sortable(fields, {
       animation: 150,
       handle: '.js-sortable-handle',
       draggable: '.nested-fields',
       onEnd: () => {
-        document
-          .querySelectorAll('.js-step-position-input')
-          .forEach((item, i) => {
-            item.value = i + 1;
-          });
+        fields.querySelectorAll('.js-position-input').forEach((item, i) => {
+          item.value = i + 1;
+        });
       }
     });
+  });
 });
