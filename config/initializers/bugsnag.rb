c9b47a41f266f64f96c7cb480f207ef9a19bcda2
James Reading <jamesreading473@gmail.com>
2020-10-25 09:27:06 +0000

    Add bugsnag release stage config

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,5 +1,6 @@
-if ENV['BUGSNAG_API_KEY'] && Rails.env.production?
+if ENV['BUGSNAG_API_KEY']
   Bugsnag.configure do |config|
     config.api_key = ENV['BUGSNAG_API_KEY']
+    config.release_stage = ENV['BUGSNAG_RELEASE_STAGE'] || 'production'
   end
 end
