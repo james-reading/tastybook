ce7fcb624af85a7b7e74b6713c6e059b2901074f
James Reading <jamesreading473@gmail.com>
2020-11-01 19:41:29 +0000

    Add pulse when moving sortable elements

--- app/webpack/javascript/modules/sortable.js
+++ app/webpack/javascript/modules/sortable.js
@@ -5,13 +5,19 @@ document.addEventListener('turbolinks:load', () => {
 
   sortableFields.forEach(fields => {
     new Sortable(fields, {
-      animation: 150,
       handle: '.js-sortable-handle',
       draggable: '.nested-fields',
       onEnd: () => {
         fields.querySelectorAll('.js-position-input').forEach((item, i) => {
           item.value = i + 1;
         });
+      },
+      onChange: event => {
+        const input = event.item.querySelector(
+          'input.form-control, textarea.form-control'
+        );
+
+        input.classList.add('tb-pulse');
       }
     });
   });
--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,7 +1,12 @@
 require 'vcr'
 
+driver_hosts = Webdrivers::Common.subclasses.map do |driver|
+  URI(driver.base_url).host
+end
+
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
   config.ignore_localhost = true
+  config.ignore_hosts(*driver_hosts)
 end
